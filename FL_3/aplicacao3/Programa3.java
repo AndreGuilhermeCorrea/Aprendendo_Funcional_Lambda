3a27da1916b0a38e8bfbd5d3e3e24c66289a5492
AndreGuilhermeCorrea <de_correa@msn.com>
2022-07-07 11:52:43 -0300

    Predicate

--- FL_3/aplicacao3/Programa3.java
+++ FL_3/aplicacao3/Programa3.java
@@ -2,13 +2,15 @@ package aplicacao3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entidades3.Produtos3;
-import utilitario3.Produtos3Predicado;
 
 public class Programa3 {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
 
 		List<Produtos3> list3 = new ArrayList<>();
 		
@@ -18,16 +20,42 @@ public class Programa3 {
 		list3.add(new Produtos3("Lenço Umedecido", 80.90));
 		
 		
+		//Predicate
+		//é uma interface genercis parametrizada com tipo t que contem apenas um método abstrato chamado test que resebe um objeto tipo t retornando um boolean
+		
+		
+		//VERSOES PREDICATE
+		//1ª VERSÃO
 		//removendo item da lista com expressão lambda e predicado		
 		//list3.removeIf(p -> p.getPreco3() >= 150);
 		
+		//2ª VERSÃO
 		//removendo item da lista com expressão lambda e predicado implementado na classe Produtos3Predicado
-		list3.removeIf(new Produtos3Predicado());
+		//list3.removeIf(new Produtos3Predicado());
+		
+		//3ª VERSÃO
+		//referencia para método utilizando método estatico
+		//list3.removeIf(Produtos3::estaticoProdutos3Predicado);
+
+		//4ª VERSÃO
+		//referencia para método utilizando método NÃO estatico
+		//list3.removeIf(Produtos3::noEstaticoProdutos3Predicado);
+		
+		//5ª VERSÃO
+		//expressão lambda declarada
+		//sem referencia de método, variável recebendo a expressão lambda (função anonima que recebe como argumento um produto p3 resultando no boolean 
+		//double min3 = 150;
+		//Predicate<Produtos3> pred = p3 -> p3.getPreco3() >= min3;
+		//list3.removeIf(pred);
+		
+		//6ª VERSÃO
+		//expressão lambda inline, ou seja diretamente como argumento do remove if,
+		list3.removeIf( p3 -> p3.getPreco3() >= 150);
 		
 		
 		
-		for (Produtos3 p : list3) {
-			System.out.println(p);
+		for (Produtos3 p3 : list3) {
+			System.out.println(p3);
 		}
 		
 
--- FL_3/entidades3/Produtos3.java
+++ FL_3/entidades3/Produtos3.java
@@ -27,9 +27,25 @@ public class Produtos3 {
 		this.preco3 = preco3;
 	}
 
+	//referencia para método utilizando método estatico
+	//método que pega um produto e retorna um boolean
+	//predicado de produto estático, recebendo um produto como argumento fazendo a mesma lógica da interface sendo apenas o método com a lógica da interface implementada
+	//ou seja não será uma classe separada que implementa uma interface 
+	public static boolean estaticoProdutos3Predicado(Produtos3 p3) {
+		return p3.getPreco3() >= 100.00;
+	}
+	
+	//referencia para método utilizando método NÃO estatico "DA INSTANCIA"
+	//método que trabalha com o prorpio produto onde se encontra
+	//método NÃO estatico s/ argumento fazendo a mesma lógica da interface MAS ACESSANDO O ATRIBUTO DO OBJETO ONDE SE ENCONTRA
+	//ou seja não será uma classe separada que implementa uma interface 
+	public boolean noEstaticoProdutos3Predicado() {
+		return preco3 >= 100.00;
+	}
+	
 	@Override
 	public String toString() {
-		return "Produtos3 [nome3=" + nome3 + ", preco3=" + preco3 + "]";
+		return (nome3 + ": " + preco3);
 	}
 
 
