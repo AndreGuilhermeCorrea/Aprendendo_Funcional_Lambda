424836c56d0df9e9125d5addc6b52781f8b02fdb
AndreGuilhermeCorrea <de_correa@msn.com>
2022-07-08 09:06:11 -0300

    Lambda e Funcional Exercicio

--- fl_8/aplicacao8/Programa8.java
+++ fl_8/aplicacao8/Programa8.java
@@ -15,11 +15,10 @@ import entidades8.Produtos8;
 public class Programa8 {
 
 	/*
-	 * Fazer um programa para ler os dados (nome, email e salário) de funcionários a
-	 * partir de um arquivo em formato .csv. Em seguida mostrar, em ordem
-	 * alfabética, o email dos funcionários cujo salário seja superior a um dado
-	 * valor fornecido pelo usuário. Mostrar também a soma dos salários dos
-	 * funcionários cujo nome começa com a letra 'M'.
+	 * Fazer um programa para ler um conjunto de produtos a partir de um arquivo em
+	 * formato .csv (suponha que exista pelo menos um produto). Em seguida mostrar o
+	 * preço médio dos produtos. Depois, mostrar os nomes, em ordem decrescente, dos
+	 * produtos que possuem preço inferior ao preço médio.
 	 */
 
 	public static void main(String[] args) {
@@ -40,39 +39,33 @@ public class Programa8 {
 				list8.add(new Produtos8(campos8[0], Double.parseDouble(campos8[1])));
 				line8 = br8.readLine();
 			}
-			
-			
-			//pipeline para encontra a soma dos valores
-			//conversão da lista pra stream
-			double media8 = list8.stream()
-					//geração de novo stream com preço dos produtos
-					.map(p8 -> p8.getPreco8())
-					//funcao reduce que permite a soma e a divisão da lista (size)
-					.reduce(0.0, (x,y) -> x + y) / list8.size();
-			
-			System.out.println("Média de preço dos produtos: " + String.format("%.2f", media8));
-			
-			
-			//função que compara duas strings independente de maiusculas e minusculas
-			Comparator<String> comp8 = (s8_1, s8_2) -> s8_1.toUpperCase().compareTo(s8_2.toUpperCase());
-			
-			List<String> nomes8 = list8.stream()
-				//pipeline filtro com todos abaixo da média
-				.filter(p8 -> p8.getPreco8() < media8)	
-				//.map para acessar o nome de cada produto que foi filtrado e criar uma nova stream
-				.map(p8 -> p8.getNome8())
-				//organizar os elementos de forma decrescente(reversed ou o comparator reverso)
-				.sorted(comp8.reversed())
-				//transformar novamente a stream em lista
-				.collect(Collectors.toList());
-			
-			
-			//impressão dos nomes na tela
-			nomes8.forEach(System.out::println);
 
-			
-			
-					
+			// pipeline para encontra a soma dos valores
+			// conversão da lista pra stream
+			double media8 = list8.stream()
+					// geração de novo stream com preço dos produtos
+					.map(p8 -> p8.getPreco8())
+					// funcao reduce que permite a soma e a divisão da lista (size)
+					.reduce(0.0, (x, y) -> x + y) / list8.size();
+
+			System.out.println("Média de preço dos produtos: " + String.format("%.2f", media8));
+
+			// função que compara duas strings independente de maiusculas e minusculas
+			Comparator<String> comp8 = (s8_1, s8_2) -> s8_1.toUpperCase().compareTo(s8_2.toUpperCase());
+
+			List<String> nomes8 = list8.stream()
+					// pipeline filtro com todos abaixo da média
+					.filter(p8 -> p8.getPreco8() < media8)
+					// .map para acessar o nome de cada produto que foi filtrado e criar uma nova
+					// stream
+					.map(p8 -> p8.getNome8())
+					// organizar os elementos de forma decrescente(reversed ou o comparator reverso)
+					.sorted(comp8.reversed())
+					// transformar novamente a stream em lista
+					.collect(Collectors.toList());
+
+			// impressão dos nomes na tela
+			nomes8.forEach(System.out::println);
 
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
